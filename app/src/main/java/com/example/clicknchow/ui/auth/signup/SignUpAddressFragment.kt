b7bfd0a1a67568b01eca6c188871b5bf81688346
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-01-15 13:33:43 +0700

    create signup page part 2

---
+++ app/src/main/java/com/example/clicknchow/ui/auth/signup/SignUpAddressFragment.kt
@@ -0,0 +1,30 @@
+package com.example.clicknchow.ui.auth.signup
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.clicknchow.databinding.FragmentSignUpAddressBinding
+import com.example.clicknchow.databinding.FragmentSignUpBinding
+
+class SignUpAddressFragment : Fragment() {
+
+    private var _binding: FragmentSignUpAddressBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentSignUpAddressBinding.inflate(layoutInflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
