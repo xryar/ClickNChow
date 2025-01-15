fd190d68227d2706edbdeb1d5ab08665b6539ee8
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-01-15 13:34:08 +0700

    remove unused import

--- app/src/main/java/com/example/clicknchow/ui/auth/signup/SignUpAddressFragment.kt
+++ app/src/main/java/com/example/clicknchow/ui/auth/signup/SignUpAddressFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.clicknchow.databinding.FragmentSignUpAddressBinding
-import com.example.clicknchow.databinding.FragmentSignUpBinding
 
 class SignUpAddressFragment : Fragment() {
 
