bae54979a00c88f4b1aba65a55eb0b74aabfeaf3
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-01-16 10:58:18 +0700

    remove unused import

--- app/src/main/java/com/example/clicknchow/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/example/clicknchow/ui/auth/AuthActivity.kt
@@ -3,7 +3,6 @@ package com.example.clicknchow.ui.auth
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavOptions
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import com.example.clicknchow.R
 import com.example.clicknchow.databinding.ActivityAuthBinding
--- app/src/main/java/com/example/clicknchow/ui/auth/signin/SignInFragment.kt
+++ app/src/main/java/com/example/clicknchow/ui/auth/signin/SignInFragment.kt
@@ -1,13 +1,11 @@
 package com.example.clicknchow.ui.auth.signin
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.clicknchow.R
+import androidx.fragment.app.Fragment
 import com.example.clicknchow.databinding.FragmentSignInBinding
 import com.example.clicknchow.ui.auth.AuthActivity
 
