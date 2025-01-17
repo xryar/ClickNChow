ebebc50fed24ecfeb67169b8e846aea398af3100
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-01-17 18:57:44 +0700

    make rv show data

--- app/src/main/java/com/example/clicknchow/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/clicknchow/ui/home/HomeFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.example.clicknchow.databinding.FragmentHomeBinding
+import com.example.clicknchow.model.dummy.HomeModel
 
 class HomeFragment : Fragment() {
 
@@ -14,6 +17,8 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private var foodList: ArrayList<HomeModel> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,6 +30,27 @@ class HomeFragment : Fragment() {
         val root: View = binding.root
 
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initDataDummy()
+        val adapter = HomeAdapter(foodList)
+        val layoutManager: LayoutManager = LinearLayoutManager(
+            context,
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        binding.rvFoodHorizontal.layoutManager = layoutManager
+        binding.rvFoodHorizontal.adapter = adapter
+    }
+
+    private fun initDataDummy() {
+        foodList = ArrayList()
+        foodList.add(HomeModel("Cherry Healthy", "", 5f))
+        foodList.add(HomeModel("Burger Tamayo", "", 4f))
+        foodList.add(HomeModel("Bwang Puttie", "", 4.5f))
     }
 
     override fun onDestroyView() {
