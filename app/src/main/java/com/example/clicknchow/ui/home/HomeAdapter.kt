cab7bfaba563c08df6edd1773727e46444a54a0c
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-01-17 14:56:31 +0700

    create adapter and data dummy

---
+++ app/src/main/java/com/example/clicknchow/ui/home/HomeAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.clicknchow.ui.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.clicknchow.databinding.ItemFoodHorizontalBinding
+import com.example.clicknchow.model.dummy.HomeModel
+
+class HomeAdapter(private val listData: List<HomeModel>) :
+    RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
+
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    class ViewHolder(var binding: ItemFoodHorizontalBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemFoodHorizontalBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val data = listData[position]
+        holder.binding.tvTitle.text = data.title
+        holder.binding.rbFood.rating = data.rating
+        Glide.with(holder.itemView.context)
+            .load(data.src)
+            .into(holder.binding.ivProduct)
+        holder.itemView.setOnClickListener {
+            onItemClickCallback.onItemClicked(listData[holder.adapterPosition])
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return listData.size
+    }
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: HomeModel)
+    }
+}
