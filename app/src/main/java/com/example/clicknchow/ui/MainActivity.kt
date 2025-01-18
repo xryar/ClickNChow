19500ccf6c84480e96936db79003e723f3be1bbe
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-01-18 12:58:01 +0700

    fix toolbar showing in MainActivity and change fragment to FragmentContainerView

--- app/src/main/java/com/example/clicknchow/ui/MainActivity.kt
+++ app/src/main/java/com/example/clicknchow/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.clicknchow.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.clicknchow.R
 import com.example.clicknchow.databinding.ActivityMainBinding
@@ -20,7 +21,9 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.navController
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         navView.setupWithNavController(navController)
