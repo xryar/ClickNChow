a06d587695006f543f447f5e58ac4904c66e6c8f
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-01-19 10:14:29 +0700

    make adapter for list food vertical and IDR currency format

---
+++ app/src/main/java/com/example/clicknchow/ui/home/customtab/newtaste/HomeNewTasteAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.clicknchow.ui.home.customtab.newtaste
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.clicknchow.databinding.ItemFoodVerticalBinding
+import com.example.clicknchow.model.dummy.HomeVerticalModel
+import com.example.clicknchow.utils.Helpers.formatPrice
+
+class HomeNewTasteAdapter(private val listData: List<HomeVerticalModel>) :
+    RecyclerView.Adapter<HomeNewTasteAdapter.ViewHolder>() {
+
+    class ViewHolder(var binding: ItemFoodVerticalBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemFoodVerticalBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val data = listData[position]
+        holder.binding.tvFoodTitle.text = data.title
+        holder.binding.tvFoodPrice.formatPrice(data.price)
+        holder.binding.rbFood.rating = data.rating
+        Glide.with(holder.itemView.context)
+            .load(data.src)
+            .into(holder.binding.ivFood)
+        holder.itemView.setOnClickListener {
+            Toast.makeText(holder.itemView.context, data.title, Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return listData.size
+    }
+
+}
---
+++ app/src/main/java/com/example/clicknchow/utils/Helpers.kt
@@ -0,0 +1,17 @@
+package com.example.clicknchow.utils
+
+import android.widget.TextView
+import java.text.DecimalFormat
+
+object Helpers {
+
+    fun TextView.formatPrice(value: String) {
+        this.text = getCurrencyIDR(java.lang.Double.parseDouble(value))
+    }
+
+    private fun getCurrencyIDR(price: Double): String {
+        val format = DecimalFormat("#,###,###")
+        return "IDR " + format.format(price).replace(",".toRegex(), ".")
+    }
+
+}
