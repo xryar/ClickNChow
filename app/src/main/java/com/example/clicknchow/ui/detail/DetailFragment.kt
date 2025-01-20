b608a41995b4bb9de37b4e7354abb4817ddc0878
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-01-20 20:47:28 +0700

    fix ui

--- app/src/main/java/com/example/clicknchow/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/clicknchow/ui/detail/DetailFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.example.clicknchow.R
 import com.example.clicknchow.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
@@ -20,6 +22,14 @@ class DetailFragment : Fragment() {
         val view = binding.root
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnOrderNow.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_payment)
+        }
     }
 
     override fun onDestroy() {
