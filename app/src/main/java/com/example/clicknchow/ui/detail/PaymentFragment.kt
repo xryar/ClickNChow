6a2e5b15c5e8b155744ee871996f663e050911ee
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-01-20 15:11:21 +0700

    create payment section & add onDestroy at DetailFragment.kt

--- app/src/main/java/com/example/clicknchow/ui/detail/PaymentFragment.kt
+++ app/src/main/java/com/example/clicknchow/ui/detail/PaymentFragment.kt
@@ -1,23 +1,23 @@
 package com.example.clicknchow.ui.detail
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.example.clicknchow.databinding.FragmentDetailBinding
+import com.example.clicknchow.R
+import com.example.clicknchow.databinding.FragmentPaymentBinding
 
-class DetailFragment : Fragment() {
+class PaymentFragment : Fragment() {
 
-    private var _binding: FragmentDetailBinding? = null
+    private var _binding: FragmentPaymentBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
-        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        _binding = FragmentPaymentBinding.inflate(layoutInflater, container, false)
         val view = binding.root
 
         return view
