45eabe2d85f2e8f5f2660bd90e7be4fa11ad275d
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-01-22 11:57:18 +0700

    retrofit initialization

---
+++ app/src/main/java/com/example/clicknchow/network/Endpoint.kt
@@ -0,0 +1,7 @@
+package com.example.clicknchow.network
+
+interface Endpoint {
+
+    fun login()
+
+}
--- app/src/main/java/com/example/clicknchow/utils/Helpers.kt
+++ app/src/main/java/com/example/clicknchow/utils/Helpers.kt
@@ -1,7 +1,16 @@
 package com.example.clicknchow.utils
 
 import android.widget.TextView
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonPrimitive
+import com.google.gson.JsonSerializer
 import java.text.DecimalFormat
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
 
 object Helpers {
 
@@ -14,4 +23,23 @@ object Helpers {
         return "IDR " + format.format(price).replace(",".toRegex(), ".")
     }
 
+    fun getDefaultGson(): Gson {
+        return GsonBuilder()
+            .excludeFieldsWithoutExposeAnnotation()
+            .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+            .registerTypeAdapter(Date::class.java, JsonDeserializer { json, _, _ ->
+                val formatServer = SimpleDateFormat("", Locale.ENGLISH)
+                formatServer.timeZone = TimeZone.getTimeZone("UTC")
+                formatServer.parse(json.asString)
+            })
+            .registerTypeAdapter(Date::class.java, JsonSerializer<Date> { src, _, _ ->
+                val format = SimpleDateFormat("", Locale.ENGLISH)
+                format.timeZone = TimeZone.getTimeZone("UTC")
+                if (src != null) {
+                    JsonPrimitive(format.format(src))
+                } else {
+                    null
+                }
+            }).create()
+    }
 }
