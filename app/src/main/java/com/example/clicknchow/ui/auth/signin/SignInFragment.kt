5531ebf181288a7322e572a1dd2e2953b5c08917
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-01-26 15:24:47 +0700

    make login works

--- app/src/main/java/com/example/clicknchow/ui/auth/signin/SignInFragment.kt
+++ app/src/main/java/com/example/clicknchow/ui/auth/signin/SignInFragment.kt
@@ -8,11 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.clicknchow.ClickNChow
 import com.example.clicknchow.R
 import com.example.clicknchow.databinding.FragmentSignInBinding
 import com.example.clicknchow.model.response.login.LoginResponse
 import com.example.clicknchow.ui.MainActivity
 import com.example.clicknchow.ui.auth.AuthActivity
+import com.google.gson.Gson
 
 class SignInFragment : Fragment(), SignInContract.View {
 
@@ -35,12 +37,32 @@ class SignInFragment : Fragment(), SignInContract.View {
         super.onViewCreated(view, savedInstanceState)
         presenter = SignInPresenter(this)
 
+        if (!ClickNChow.getApp().getToken().isNullOrEmpty()) {
+            val home = Intent(activity, MainActivity::class.java)
+            startActivity(home)
+            activity?.finish()
+        } else if (ClickNChow.getApp().getToken().isNullOrEmpty()) {
+            val auth = Intent(activity, AuthActivity::class.java)
+            startActivity(auth)
+            activity?.finish()
+        }
+
         initView()
+        initDummy()
 
         binding.btnLogin.setOnClickListener {
-//            val email = binding.edEmail.text.toString()
-//            val password = binding.edPassword.text.toString()
-            presenter.submitLogin("rusdi12322@hotbarber.com", "12345678")
+            val email = binding.edEmail.text.toString()
+            val password = binding.edPassword.text.toString()
+
+            if (email.isEmpty()) {
+                binding.edEmail.error = "Silahkan masukkan Email anda"
+                binding.edEmail.requestFocus()
+            } else if (password.isEmpty()) {
+                binding.edPassword.error = "Silahkan masukkan Password anda"
+                binding.edPassword.requestFocus()
+            } else {
+                presenter.submitLogin(email, password)
+            }
         }
 
         binding.btnRegister.setOnClickListener {
@@ -51,6 +73,10 @@ class SignInFragment : Fragment(), SignInContract.View {
     }
 
     override fun onLoginSuccess(loginResponse: LoginResponse) {
+        val gson = Gson().toJson(loginResponse.user)
+        ClickNChow.getApp().setUser(gson)
+        ClickNChow.getApp().setToken(loginResponse.access_token)
+
         val login = Intent(activity, MainActivity::class.java)
         startActivity(login)
         activity?.finish()
@@ -58,6 +84,11 @@ class SignInFragment : Fragment(), SignInContract.View {
 
     override fun onLoginFailed(message: String) {
         Toast.makeText(activity, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun initDummy() {
+        binding.edEmail.setText("rusdi12322@hotbarber.com")
+        binding.edPassword.setText("12345678")
     }
 
     private fun initView() {
