73e0b10e65e2900a13a2d367b1004691c6c578b1
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-01-26 18:10:33 +0700

    create imagePicker

--- app/src/main/java/com/example/clicknchow/ui/auth/signup/SignUpFragment.kt
+++ app/src/main/java/com/example/clicknchow/ui/auth/signup/SignUpFragment.kt
@@ -1,19 +1,50 @@
 package com.example.clicknchow.ui.auth.signup
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.clicknchow.R
 import com.example.clicknchow.databinding.FragmentSignUpBinding
 import com.example.clicknchow.ui.auth.AuthActivity
+import com.github.dhaval2404.imagepicker.ImagePicker
 
 class SignUpFragment : Fragment() {
 
     private var _binding: FragmentSignUpBinding? = null
     private val binding get() = _binding!!
+    private var filePath: Uri? = null
+    private val imagePickerLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val resultCode = result.resultCode
+            val data = result.data
+
+            when (resultCode) {
+                Activity.RESULT_OK -> {
+                    filePath = data?.data
+
+                    Glide.with(this)
+                        .load(filePath)
+                        .apply(RequestOptions.circleCropTransform())
+                        .into(binding.ivUser)
+                }
+                ImagePicker.RESULT_ERROR -> {
+                    Toast.makeText(context, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    Toast.makeText(context, "Task Cancelled", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,7 +71,11 @@ class SignUpFragment : Fragment() {
 
     private fun initListener() {
         binding.ivUser.setOnClickListener {
-
+            ImagePicker.with(this)
+                .cameraOnly()
+                .createIntent { intent ->
+                    imagePickerLauncher.launch(intent)
+                }
         }
     }
 
