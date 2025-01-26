8eefaaec40b7ffff48f86c109122118477a908af
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-01-26 18:11:03 +0700

    remove unused import

--- app/src/main/java/com/example/clicknchow/ui/auth/signup/SignUpFragment.kt
+++ app/src/main/java/com/example/clicknchow/ui/auth/signup/SignUpFragment.kt
@@ -1,7 +1,6 @@
 package com.example.clicknchow.ui.auth.signup
 
 import android.app.Activity
-import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
