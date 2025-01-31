ce134e9cbffc93afd6eca03c3dc650a2ea58b45d
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-01-31 16:03:20 +0700

    remove unused import and add content description

--- app/src/main/java/com/example/clicknchow/ui/order/pastorders/PastOrderFragment.kt
+++ app/src/main/java/com/example/clicknchow/ui/order/pastorders/PastOrderFragment.kt
@@ -5,12 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.example.clicknchow.databinding.FragmentPastOrderBinding
-import com.example.clicknchow.databinding.FragmentProfileAccountBinding
-import com.example.clicknchow.model.dummy.ProfileMenuModel
-import com.example.clicknchow.ui.profile.ProfileAdapter
 
 class PastOrderFragment : Fragment() {
 
